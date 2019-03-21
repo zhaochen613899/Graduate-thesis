3ba3d813af7650fb7d4f4e937d7252ecabafb47f
123 <123@123.com>
2019-03-21 22:36:45 +0800

    20190321

--- 2-1试验数据/2第二批Q890角焊缝试验201901/1力位移曲线/Drawloaddef.py
+++ 2-1试验数据/2第二批Q890角焊缝试验201901/1力位移曲线/Drawloaddef.py
@@ -4,11 +4,12 @@ import shutil
 import matplotlib
 import xlrd
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 matplotlib.matplotlib_fname()
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 解决中文显示问题，目前只知道黑体可行
 plt.rcParams['axes.unicode_minus'] = False  # 解决负数坐标显示问题
-font_zh = {'family': 'Adobe Song Std', 'size': 12}
+font_zh = {'family': 'SimHei', 'size': 12}
 font_en = {'family': 'Times New Roman', 'size': 12}
 
 xlsFile = xlrd.open_workbook(r'力位移曲线最终版.xlsx')
@@ -46,7 +47,7 @@ plt.rcParams["figure.figsize"] = [5, 5]
 x_names = ["Deformation_B-A", "Deformation_B-C", "平均位移(mm)"]
 y_name = "荷载(kN)"
 colors = ["blue", "red", "green"]
-linestyles = [":", "-.",  "-"]
+linestyles = [":", "-.", "-"]
 allSingle = path + "/所有试件"
 if os.path.exists(allSingle):
     shutil.rmtree(allSingle)
@@ -56,17 +57,21 @@ for sheetname in all_specimen_groups:
     for specimen_name in specimen_group:
         specimen = specimen_group[specimen_name]
         index = 0
+        figure, ax = plt.subplots(figsize=(12, 8))
         for x_name in x_names:
             x_data = specimen[x_name]
             y_data = specimen[y_name]
-            plt.xlabel(x_name)  # X轴标签
-            plt.ylabel(y_name)  # Y轴标签
-            plt.title(specimen_name)  # 标题
+            font_zh["size"] = 30
+            plt.xlabel("位移(mm)", font_zh)  # X轴标签
+            plt.ylabel(y_name, font_zh)  # Y轴标签
+            plt.title(specimen_name, font_zh)  # 标题
+            plt.tick_params(labelsize=30)
+            labels = ax.get_xticklabels() + ax.get_yticklabels()
+            [label.set_fontname('Times New Roman') for label in labels]
             plt.plot(x_data, y_data, color=colors[index], linestyle=linestyles[index], mfc='w', label=x_name)
-            plt.legend()  # 让图例生效
+            plt.legend(prop=font_zh)  # 让图例生效
             index = index + 1
-            plt.savefig(allSingle + "/" + specimen_name + '.png')
-
+        plt.savefig(allSingle + "/" + specimen_name + '.png')
         plt.show()
 
 # 画单组试件=六条曲线
@@ -82,16 +87,19 @@ os.mkdir(group_path)
 for sheetname in all_specimen_groups:
     specimen_group = all_specimen_groups[sheetname]
     index = 0
+    figure, ax = plt.subplots(figsize=(12, 8))
     for specimen_name in specimen_group:
         specimen = specimen_group[specimen_name]
         x_data = specimen[x_name]
         y_data = specimen[y_name]
-        plt.xlabel(x_name)  # X轴标签
-        plt.ylabel(y_name)  # Y轴标签
-        plt.title(sheetname + "°试件力位移曲线")  # 标题
+        plt.xlabel("位移(mm)", font_zh)  # X轴标签
+        plt.ylabel(y_name, font_zh)  # Y轴标签
+        plt.tick_params(labelsize=30)
+        labels = ax.get_xticklabels() + ax.get_yticklabels()
+        [label.set_fontname('Times New Roman') for label in labels]
+        plt.title(sheetname + "°试件力位移曲线", font_zh)  # 标题
         plt.plot(x_data, y_data, mfc='w', color=colors[index], linestyle=linestyles[index], label=specimen_name)
-        plt.legend()  # 让图例生效
+        plt.legend(prop=font_zh)  # 让图例生效
         index = index + 1
-
-        plt.savefig(group_path + "/" + sheetname + '.png')
+    plt.savefig(group_path + "/" + sheetname + '.png')
     plt.show()
